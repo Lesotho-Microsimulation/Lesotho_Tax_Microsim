73fe523a9aa7c95160a3f0e3cbf618f82217edd3
Sebastian James <sebastiansajie@yahoo.com>
2024-12-10 00:55:19 -0500

    fixes to the repository

--- super_combo.py
+++ super_combo.py
@@ -510,15 +510,15 @@ class super_combo(tk.Frame):
         self.button_generate_revenue_policy.place(relx = self.generate_revenue_policy_button_x,
                                                     rely = self.generate_revenue_policy_button_y, anchor = "w")       
         
-        self.image1 = Image.open("world_bank.png")
-        self.image2 = self.image1.resize((700, 400), Image.ANTIALIAS)
-        self.image = ImageTk.PhotoImage(self.image2)
+        #self.image1 = Image.open("world_bank.png")
+        #self.image2 = self.image1.resize((700, 400), Image.ANTIALIAS)
+        #self.image = ImageTk.PhotoImage(self.image2)
         # "world_bank.png"
         #self.image1 = ImageTk.PhotoImage(Image.open("egypt_flag.jpg"))
     
         #image = tk.PhotoImage(file="blank.png")
-        self.pic = tk.Label(tab,image=self.image)
-        self.pic.place(relx = 0.50, rely = 0.30, anchor = "nw")
-        self.pic.image = self.image
+        #self.pic = tk.Label(tab,image=self.image)
+        #self.pic.place(relx = 0.50, rely = 0.30, anchor = "nw")
+        #self.pic.image = self.image
         return (self.button_generate_revenue_policy, self.block_widget_dict)
          
